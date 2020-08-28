6ba122b01cfb8df2640681ad443a7d3207390a9d
Bruna Moreira <brunanayaramlima@gmail.com>
2020-08-28 08:45:48 -0300

    Validate incoming content type

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 from flask import Flask
+from flask import request
 import requests
 import crawler
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Starting our API.'
 
-@app.route('/get')
+@app.route("/")
+def hello_world():
+    return "Starting our API."
+
+
+@app.route("/processo", methods=["GET"])
 def get_site():
-    r = requests.get('https://www2.tjal.jus.br/cpopg/open.do')
+    content_type = request.headers.get("Content-Type")
+    if not _is_valid_content_type(content_type):
+        print("not valid content type")
+        return "Cannot parse content type"
+
+    json_data = request.get_json(request.data)
+    num_processo = json_data.get("numProcesso")
+    print(json_data)
+
+    r = requests.get("https://www2.tjal.jus.br/cpopg/open.do")
     return crawler.get_title(r.text)
+
+
+def _is_valid_content_type(incoming):
+    return incoming == "application/json"
